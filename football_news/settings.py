72a7cc2116819bd2ddec55c9de6e47164ed475e2
aliakb-12 <alimurtadha2006@gmail.com>
2025-09-10 14:27:49 +0700

    Finish tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -31,6 +31,9 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ali-akbar45-footballnews.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = [
+    "<https://pbp.cs.ui.ac.id/ali.akbar45/footballnews>"
+]
 
 
 # Application definition
@@ -60,7 +63,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- football_news/urls.py
+++ football_news/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = 'main'
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("main.urls")),  # include the app
 ]
+
